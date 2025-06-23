af87fd96c9d15ed1b6d0cfb703e0aae0c7f68cdc
Shirayuki39 <lorddemecrius83@proton.me>
2025-06-23 09:30:48 +0900

    JamesDSP: drop SEPolicy
    * We don't need this
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- config.mk
+++ config.mk
@@ -20,9 +20,6 @@ JAMESDSP_PATH := vendor/JamesDSP
 PRODUCT_SOONG_NAMESPACES += \
    $(JAMESDSP_PATH)
 
-# SEPolicy
-BOARD_VENDOR_SEPOLICY_DIRS += $(JAMESDSP_PATH)/sepolicy/vendor
-
 PRODUCT_PACKAGES += \
     libjamesdsp \
     JamesDSPManager
